c85544af42a72edcdd76cd3167d641a7c09c075f
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2023-01-02 10:46:28 -0700

    Update Adivina el num del Dado.cpp

--- Adivina el num del Dado.cpp
+++ Adivina el num del Dado.cpp
@@ -12,29 +12,23 @@ int main()
 	setlocale(LC_ALL, "spanish");
 	SetConsoleCP(1252);
 	SetConsoleOutputCP(1252);
-
 	srand(time(0));
-	int num = 0,num_random;
-	std::string inicio = "no",play1 = "si";
+
+	int num = 0, num_random;
+	std::string inicio = "no";
 
 	std::cout << "----------------ADIVINA EL NUMERO DEL DADO!----------------\n\n"; Sleep(1000);
-	std::cout << "El juego consiste en escribir un numero del 1 al 6\n\n"; Sleep(1500); 
-	std::cout << "Adivina el numero del dado!\n"; Sleep(1000);
+	std::cout << "El juego consiste en escribir un numero del 1 al 6\n\n"; Sleep(1500);
+	std::cout << "¡Adivina el numero del dado!\n"; Sleep(1000);
 
-	do{
+	do {
+		std::cout << "\nEscribe un numero del 1 al 6: "; std::cin >> num;
 
-		while (num < 1 || num >6)
-		{
-			std::cout << "\nEscribe un numero del 1 al 6: "; std::cin >> num;
+		std::cout << "\nLANZAMOS EL DADO!"; Sleep(1000); std::cout << "\nY."; Sleep(300); std::cout << "."; Sleep(300); std::cout << "."; Sleep(300); std::cout << ".\n"; Sleep(300);
 
-			std::cout << "\nDato erroneo, digite un numero entre 1 y 6\n";
-		}
+		num_random = rand() % 6 + 1;
 
-	std::cout << "\nLANZAMOS EL DADO!"; Sleep(1000); std::cout << "\nY."; Sleep(300); std::cout << "."; Sleep(300); std::cout << "."; Sleep(300); std::cout << ".\n"; Sleep(300);
-
-	num_random = rand() % 6 + 1;
-	
-	std::cout << "\nEl numero que toco fue: " << num_random << "!\n"; Sleep(1000);
+		std::cout << "\nEl numero que toco fue: " << num_random << "!\n"; Sleep(1000);
 
 		if (num == num_random)
 		{
@@ -45,9 +39,9 @@ int main()
 			std::cout << "\nMala suerte! :c \n\n";
 		}
 
-	std::cout << "Quieres volver a jugar? \n"; std::cout << "Escriba si o no para continuar: \n\n"; std::cin >> inicio;
+		std::cout << "Quieres volver a jugar? \n"; std::cout << "Escriba si o no para continuar: \n\n"; std::cin >> inicio;
 
-	}while (inicio == "Si"|| inicio == "si");
+	} while (inicio == "Si" || inicio == "si");
 
 	std::cout << std::endl;
 
