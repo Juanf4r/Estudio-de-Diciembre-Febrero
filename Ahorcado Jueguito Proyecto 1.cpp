08650125113649a4c558ddda30e102e1f1642080
Juan Fernando Aispuro <Juanfernand2004@hotmail.com>
2023-01-04 20:27:58 -0700

    Update Ahorcado Jueguito Proyecto 1.cpp

--- Ahorcado Jueguito Proyecto 1.cpp
+++ Ahorcado Jueguito Proyecto 1.cpp
@@ -23,16 +23,17 @@ palabra3 = { "VideoJuego" };
 int main()
 {
 	system("CLS");
+	setlocale(LC_ALL,"");
 
 	char band = 'F';
 	char letras_error[10] = {};
 	char letra;
 
-	std::cout << "Bienvenido al Juego del Ahorcado :)\n");
-	std::cout << "Si quieres salvar a esta persona deberas adivinar la palabra del juego\n");
-	std::cout << "Por cada intento que hagas estara mas cerca de morir\n");
-	std::cout << "Así que si te equivocas mucho pierdes :))\n");
-	std::cout <<"QUE COMIENCE EL JUEGO\n\n");
+	std::cout << "Bienvenido al Juego del Ahorcado :)\n";
+	std::cout << "Si quieres salvar a esta persona deberas adivinar la palabra del juego\n";
+	std::cout << "Por cada intento que hagas estara mas cerca de morir\n";
+	std::cout << "Así que si te equivocas mucho pierdes :))\n";
+	std::cout << "QUE COMIENCE EL JUEGO\n\n";
 
 	std::cout << "------------------------------------------------\n";
 	std::cout << "Letras escritas anteriormente: " << letras_error << "\n";
@@ -43,8 +44,9 @@ int main()
 	std::cout << "                                                \n";
 	std::cout << "                                                \n";
 	std::cout << "                                                \n";
+	std::cout << "                                                \n";
+	std::cout << "                                                \n";
 	std::cout << "Escribe una letra: "; std::cin >> letra;
-		
 
 
 	system("pause");
